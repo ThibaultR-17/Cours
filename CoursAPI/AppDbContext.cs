0102b7f83bc85064783cf65bb29f86a8077a75c7
Thibault <thibault.roux17@gmail.com>
2026-06-17 15:16:18 +0200

    Gros Rework avec une meilleure compréhension du sujet et des possibilités ajout des fichiers manquants

---
+++ CoursAPI/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoursAPI
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Models.TodoItem> TodoItems { get; set; }
+
+
+        public DbSet<Models.ProjetItem> ProjetItems { get; set; }
+    }
+}
